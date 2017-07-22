4a89fcef5f2c16fbfd4f641a2d16115cd3deaaae
Hyrum Steffensen <hyrum.stefensen@gmail.com>
2017-07-22 02:18:11 -0700

    mend

--- src/client/ui/_dropdown_menu_test.jsx
+++ src/client/ui/_dropdown_menu_test.jsx
@@ -3,7 +3,7 @@
 
   var ReactDOMServer = require("./../react-dom-server-15.3.1.js");
   //var TestUtils = React.addons.TestUtils;
-  var DropdownMenu = require('./dropdown_menu');  
+  var DropdownMenu = require('./dropdown_menu');
 
   describe("Dropdown Menu", function() {
     it("tests static HTML", function() {
