f7273053c97c2ca38f16e3aef2f7784ca074994d
Hyrum Steffensen <hyrum.stefensen@gmail.com>
2017-07-28 11:21:24 -0700

    refactor

--- src/client/ui/_convert_button_test.jsx
+++ src/client/ui/_convert_button_test.jsx
@@ -2,31 +2,38 @@
   "use strict";
 
   var ReactDOMServer = require("./../react-dom-server-15.3.1.js");
-//  var ConvertButton = require("./convert_button.js");
   var InputBox = require("./input_box.js");
   var TestUtils = React.addons.TestUtils;
 
   describe("Convert Button", function() {
+    var inputBox;
+
+    beforeEach(function() {
+      inputBox = TestUtils.renderIntoDocument(<InputBox />);
+    });
 
     it("is disabled by default", function() {
-      var inputBox = TestUtils.renderIntoDocument(<InputBox />);
       expect((TestUtils.findRenderedDOMComponentWithTag(inputBox, "button").disabled)).to.be(true);
     });
 
     it("is enabled when user enters text", function() {
-      var inputBox = TestUtils.renderIntoDocument(<InputBox />);
-      var input = TestUtils.findRenderedDOMComponentWithTag(inputBox, "input");
-      TestUtils.Simulate.change(input, { target: {value: "foo"} });
-      expect((TestUtils.findRenderedDOMComponentWithTag(inputBox, "button").disabled)).to.be(false);
+      TestUtils.Simulate.change(input(), { target: {value: "foo"} });
+      expect(button()).to.be(false);
     });
 
     it("is disabled when user deletes all text", function() {
-      var inputBox = TestUtils.renderIntoDocument(<InputBox />);
-      var input = TestUtils.findRenderedDOMComponentWithTag(inputBox, "input");
-      TestUtils.Simulate.change(input, { target: {value: ""} });
-      expect(TestUtils.findRenderedDOMComponentWithTag(inputBox, "input").value).to.equal("");
-      expect((TestUtils.findRenderedDOMComponentWithTag(inputBox, "button").disabled)).to.be(true);
+      TestUtils.Simulate.change(input(), { target: {value: ""} });
+      expect(input().value).to.equal("");
+      expect(button()).to.be(true);
     });
+
+    function input() {
+      return TestUtils.findRenderedDOMComponentWithTag(inputBox, "input");
+    }
+
+    function button() {
+      return TestUtils.findRenderedDOMComponentWithTag(inputBox, "button").disabled;
+    }
   });
 
 })();
